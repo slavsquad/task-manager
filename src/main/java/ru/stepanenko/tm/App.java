b29f43c7d0d33546a694cff3589963f5a3a831a2
slavsquad <slavsquad@yandex.ru>
2019-04-12 14:19:49 +0300

    task SE-03 and SE-04 complite!

--- src/main/java/ru/stepanenko/tm/App.java
+++ src/main/java/ru/stepanenko/tm/App.java
@@ -2,9 +2,6 @@ package ru.stepanenko.tm;
 
 import ru.stepanenko.tm.config.Bootstrap;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         Bootstrap bootstrap = new Bootstrap();
