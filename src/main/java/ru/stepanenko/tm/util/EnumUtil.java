1876e721741d153e0d88f8c260d627d26edfb123
slavsquad <slavsquad@yandex.ru>
2019-04-23 18:44:04 +0300

    1 Add Enumeration Status 2 Add commands of sorting for Task and Project

--- src/main/java/ru/stepanenko/tm/util/EnumUtil.java
+++ src/main/java/ru/stepanenko/tm/util/EnumUtil.java
@@ -3,16 +3,26 @@ package ru.stepanenko.tm.util;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.stepanenko.tm.enumerate.Role;
+import ru.stepanenko.tm.enumerate.Status;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class RoleUtil {
+public class EnumUtil {
     static public Role stringToRole(@Nullable final String string) {
         @NotNull
         Map<String, Role> userRoles = new HashMap<>(2);
         userRoles.put("admin", Role.ADMINISTRATOR);
         userRoles.put("user", Role.USER);
         return userRoles.get(string);
+    }
+
+    static public Status stringToStatus(@Nullable final String string){
+        @NotNull
+        Map<String, Status> statusMap = new HashMap<>(3);
+        statusMap.put("in process", Status.INPROCESS);
+        statusMap.put("planned", Status.PLANNED);
+        statusMap.put("done", Status.DONE);
+        return statusMap.get(string);
     }
 }
