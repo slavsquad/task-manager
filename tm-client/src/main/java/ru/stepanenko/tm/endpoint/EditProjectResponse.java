c16d0b6300d8e574ab1713d968fad92c2f32a3f0
slavsquad <slavsquad@yandex.ru>
2019-05-13 01:35:12 +0300

    Task SE-13 is complite!

---
+++ tm-client/src/main/java/ru/stepanenko/tm/endpoint/EditProjectResponse.java
@@ -0,0 +1,62 @@
+
+package ru.stepanenko.tm.endpoint;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for editProjectResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="editProjectResponse"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="return" type="{http://endpoint.tm.stepanenko.ru/}project" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "editProjectResponse", propOrder = {
+    "_return"
+})
+public class EditProjectResponse {
+
+    @XmlElement(name = "return")
+    protected Project _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Project }
+     *     
+     */
+    public Project getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Project }
+     *     
+     */
+    public void setReturn(Project value) {
+        this._return = value;
+    }
+
+}
